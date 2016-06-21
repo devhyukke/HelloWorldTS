065c7aac171f0f8a7b5b1eb2416c049999f1d2e5
hyukke <yu_suke.talesofse@tempo.ocn.ne.jp>
2016-06-21 18:10:44 +0900

    Fixed #21 登録・更新・削除・ログアウトの前に確認モーダルダイアログを出力するように修正

---
+++ hello-wts/src/main/resources/static/js/application.js
@@ -0,0 +1,34 @@
+$(function() {
+
+	$(document).on('click', '.confirm-modal-dialog[data-target]', function(e) {
+
+		$element = $(this);
+
+		var event = null;
+		if ($element.is('*[type=submit]')) {
+			event = function() {
+            	var $form = $element.closest('form');
+            	$form.append($('input[type=hidden]').attr('name', $element.attr('name')));
+            	$form.submit();
+			};
+		} else if ($element.is('a')) {
+			// hrefを指定すると遷移されてしまうためカスタムデータ属性で参照先を指定
+			var href = $element.data('href');
+			event = function() {
+				location.href = href;
+			};
+		}
+
+		var target = $element.data('target');
+		var $ok = $(target).find('.confirm-modal-dialog-ok'),
+			$cancel = $(target).find('.confirm-modal-dialog-cancel');
+		$ok.off('click')
+			.on('click', event);
+		$cancel.on('click', function() {
+			$ok.off('click', event);
+		});
+
+		// サブミットへの伝播を停止
+        e.preventDefault();
+	});
+});
