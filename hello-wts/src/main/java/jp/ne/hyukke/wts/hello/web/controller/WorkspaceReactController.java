6528065492a48e568c7fc43256007592be370c03
hyukke <yu_suke.talesofse@tempo.ocn.ne.jp>
2016-08-04 20:35:46 +0900

    react.jsのチュートリアルを追加

---
+++ hello-wts/src/main/java/jp/ne/hyukke/wts/hello/web/controller/WorkspaceReactController.java
@@ -0,0 +1,29 @@
+package jp.ne.hyukke.wts.hello.web.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * React.jsのワークスペースを操作するコントローラクラス.
+ *
+ * @author hyukke
+ */
+@Controller
+@PreAuthorize("hasAnyRole('ROLE_SYSTEM_ADMIN', 'ROLE_USER_MANAGER', 'ROLE_USER')")
+@RequestMapping("workspace/reactjs")
+public class WorkspaceReactController {
+
+    /**
+     * ビューを表示する.
+     *
+     * @param model モデル
+     * @return ビュー
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public String show() {
+
+        return "workspace/reactjs/index";
+    }
+}
